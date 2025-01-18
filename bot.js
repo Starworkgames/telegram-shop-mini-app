d14831851867cafc797aeee5be73da104350a881
Starworkgames <104005524+Starworkgames@users.noreply.github.com>
2025-01-19 01:52:36 +0400

    Create bot.js

---
+++ bot.js
@@ -0,0 +1,31 @@
+const TelegramBot = require('node-telegram-bot-api');
+
+// Токен, который вы получили от BotFather
+const token = '7190268129:AAENp4cRz3G_e9JWXZjvjFm1-36KEkm4Ezs';
+
+// Создаем нового бота
+const bot = new TelegramBot(token, { polling: true });
+
+// Когда пользователь отправляет команду /start
+bot.onText(/\/start/, (msg) => {
+  const chatId = msg.chat.id;
+  const username = msg.chat.username;
+
+  // Создаем инлайн-кнопку с ссылкой на ваше приложение
+  const options = {
+    reply_markup: {
+      inline_keyboard: [
+        [
+          {
+            text: 'Перейти в магазин',
+            url: 'https://starworkgames.github.io/telegram-shop-mini-app/'  // Замените на свою ссылку
+          }
+        ]
+      ]
+    }
+  };
+
+  // Отправляем сообщение с кнопкой
+  bot.sendMessage(chatId, `Привет, ${username}! Нажмите кнопку ниже, чтобы перейти в магазин.`, options);
+});
+
